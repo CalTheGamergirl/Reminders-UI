58ddb494680d56a8850a7f2790cb7c1b3f023c82
CalTheGamerGirl <ecash.peterson@gmail.com>
2022-08-10 13:04:05 +0000

    Initial Commit

---
+++ Task UI/Task_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_UIApp.swift
+//  Task UI
+//
+//  Created by Cal Cash-Peterson on 8/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Task_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
